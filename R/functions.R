9e19ebb39d79cd4fcd225bde9476976ac8f5a1ca
Simon <simon@elisya.org>
2025-04-01 21:08:45 +0100

    correct minor bug

--- R/functions.R
+++ R/functions.R
@@ -466,7 +466,7 @@ return(dist)}}
 summary_maker <- function(d){
     x <- as.data.frame(table(d$SiteID)); names(x) <- c('SiteID','count')
     x <- merge(x,unique(d[,1:3]),by='SiteID')
-    xi$code[x$count==1] <- 1
+    x$code[x$count==1] <- 1
     x$code[x$count==2] <- 2
     posts <- floor(unique(quantile(x$count[!x$count%in%c(1,2)])))
     N <- length(posts)-1
